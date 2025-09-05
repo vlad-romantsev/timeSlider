68eb2e6468b7a0bd16879dc2558f727c4fdeb183
vldpvnnk <vlad.romantsev@inbox.ru>
2025-09-05 15:25:49 +0700

    feat: add NavigationButtons component and timeSegments in context

---
+++ src/components/EventSlider/icons/RightArrow.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+const RightArrow = () => {
+    return (
+        <svg 
+            width="8" 
+            height="12" 
+            viewBox="0 0 8 12" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M1 1L6 6L1 11" stroke="#3877EE" strokeWidth="2"/>
+        </svg>
+    )
+}
+
+export default RightArrow;
---
+++ src/components/TimeSlider/NavigationButtons.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import LeftArrow from "../EventSlider/icons/LeftArrow";
+import RightArrow from "../EventSlider/icons/RightArrow";
+
+interface NavigationButtonsProps {
+  activeIndex: number;
+  length: number;
+  onNext: () => void;
+  onPrev: () => void;
+  onDotClick: (index: number) => void;
+}
+
+const NavigationButtons: React.FC<NavigationButtonsProps> = ({
+  activeIndex,
+  length,
+  onNext,
+  onPrev,
+  onDotClick
+}) => {
+  const opacityLeftArrow = activeIndex !== 0 ? 1 : 0.5;
+  const opacityRightArrow = activeIndex !== length - 1 ? 1 : 0.5;
+
+  return (
+    <>
+      <div className="buttons-wrapper">
+        <button
+          className="prev"
+          onClick={onPrev}
+          disabled={activeIndex === 0}
+          aria-label="Предыдущий сегмент"
+          style={{
+            opacity: opacityLeftArrow,
+            cursor: opacityLeftArrow === 1 ? "pointer" : "auto",
+          }}
+        >
+          <LeftArrow />
+        </button>
+        <button
+          className="next"
+          onClick={onNext}
+          disabled={activeIndex === length - 1}
+          aria-label="Следующий сегмент"
+          style={{
+            opacity: opacityRightArrow,
+            cursor: opacityRightArrow === 1 ? "pointer" : "auto",
+          }}
+        >
+          <RightArrow />
+        </button>
+      </div>
+      <div className="mobile-nav-wrapper">
+          <div className="arrows-block">
+            <button
+              className="nav-arrow"
+              onClick={onPrev}
+              disabled={activeIndex === 0}
+              aria-label="Предыдущий"
+            >
+              <LeftArrow />
+            </button>
+            <button
+              className="nav-arrow"
+              onClick={onNext}
+              disabled={activeIndex === length - 1}
+              aria-label="Следующий"
+            >
+              <RightArrow />
+            </button>
+          </div>
+          <div className="dots-block">
+            {Array.from({ length }).map((_, index) => (
+              <button
+                key={index}
+                className={`dot ${index === activeIndex ? "active" : ""}`}
+                onClick={() => onDotClick(index)}
+                aria-label={`Go to segment ${index + 1}`}
+                aria-current={index === activeIndex ? "true" : "false"}
+              />
+            ))}
+          </div>
+      </div>
+    </>
+
+  );
+};
+
+export default NavigationButtons;
